4d3b1f492ead868ba88a1093af91308dc247e2c2
akhare <akhare@i068.vpnclients.gwdg.de>
2014-08-24 16:04:33 +0200

    Bash file for the linux enviroment .

---
+++ GoEuroTest/run.bash
@@ -0,0 +1,6 @@
+## Welcome to location api search a test application for GoEuro
+echo "Please enter your search criteria:"
+read input
+java -jar GoEuroTest.jar $input
+echo ""
+echo "Execution is completed please review the output file "
