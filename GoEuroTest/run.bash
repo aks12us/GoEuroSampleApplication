84cd39fe9ffe190db80e4c2168a92da2cce2a9db
akhare <akhare@i068.vpnclients.gwdg.de>
2014-08-24 16:06:36 +0200

    Updated

--- GoEuroTest/run.bash
+++ GoEuroTest/run.bash
@@ -1,6 +1,7 @@
-## Welcome to location api search a test application for GoEuro
+echo "##### Welcome to location API search a test application for GoEuro #####"
 echo "Please enter your search criteria:"
 read input
 java -jar GoEuroTest.jar $input
-echo ""
-echo "Execution is completed please review the output file "
+echo "#######################################################"
+echo "Execution is completed please review the output file ."
+echo "#######################################################"
